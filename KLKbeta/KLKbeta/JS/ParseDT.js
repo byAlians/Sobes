c7858936b6b77af7a104bc74927ddfba5f1cf1dc
Глеб Графа <byalians@gmail.com>
2019-06-14 17:38:54 +0300

    parse

---
+++ KLKbeta/KLKbeta/JS/ParseDT.js
@@ -0,0 +1,7 @@
+﻿$(function parse(){
+    alert($("td").css("background-color"));
+    $("td").css("background-color", "blue");
+    var dataTable = this.getElementsByName("FactTable");
+    var tdOfTable = dataTable.length;
+    alert(tdOfTable)
+});
