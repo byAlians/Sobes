db57747ef052c529e272102f555a142e7a536311
Глеб Графа <byalians@gmail.com>
2019-06-14 11:49:20 +0300

    14.06DI

---
+++ KLKbeta/KLKbeta/Controllers/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KLKbeta.DependencyInjection.Release;
+using KLKbeta.DependencyInjection;
+using KLKbeta.Models.ModelForDB.ModelPeople;
+
+namespace KLKbeta.Controllers
+{
+    public class MainController : Controller
+    {
+        IRepository<Staff> db;
+
+        public MainController()
+        {
+            db = new SQLStaffRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+            return View(db.GetList());
+        }
+    }
+}
